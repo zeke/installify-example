0efc187908ef0741afbc20c13e1b2b2a34da462d
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-28 16:24:13 -0600

    throw some babel in the mix to make sure multiple transforms work

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const fs              = require('fs')
 const path            = require('path')
+const babelify        = require('babelify')
 const browserify      = require('browserify')
 const installify      = require('installify')
 const concat          = require('concat-stream')
 
 browserify('./lazy.js')
+  .transform('babelify', {presets: ['es2015']})
   .transform(installify)
   .bundle()
   .pipe(concat(function(buffer){
